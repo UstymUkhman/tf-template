7ecdbdd38d211cf372381b4b5f8b066dc74146d1
Ustym Ukhman <ustym.ukhman@gmail.com>
2015-11-06 08:10:54 +0100

    Directive Handler Updates

--- demo.js
+++ demo.js
@@ -1,16 +1,51 @@
 (function() {
 	angular.module('demo', ['ng.puppa'])
-		.controller('responseController', function($scope) {
-      $scope.responces = ['warning', 'angry', 'explosive'];
-      $scope.selectedResp = $scope.responces[1];
-    })
-
     .controller('directiveController', function($scope) {
-      $scope.validate = '';
-      $scope.valid    = null;
-      $scope.submit   = document.getElementById('response');
 
-      $scope.validateToPuppa = function() {
+      // Make a factory:
+      $scope.getRandBool = function() {
+        switch (parseInt(Math.random()*3)) {
+          case 0: return false;
+          case 1: return true;
+          case 2: return null;
+        }
+      };
+
+      // Make a factory:
+      $scope.getCustomOpts = function() {
+
+      };
+
+      $scope.valid        = '';
+      $scope.responces    = ['OK', '!OK', 'Custom'];
+      $scope.selectedResp = $scope.responces[1];
+
+      $scope.validate     = { 'OK': true,
+                              '!OK': [false, true, $scope.getRandBool()],
+                              'Custom': $scope.getRandBool()};
+
+      $scope.validateOpts = { 'OK': { },
+                              '!OK': {opr: '&&', ok: 'tpl/ok.tpl', notOk: 'tpl/notOk.tpl'},
+                              'Custom': $scope.getCustomOpts()};
+
+
+
+      $scope.setResponse = function(value) {
+        $scope.valid = $scope.validate[value];
+      };
+
+      /*$scope.validateToPuppa = function() {
+        $scope.validate = ;
+        $scope.ngPuppaOpts = {
+          opr: '&&',
+          ok: 'tpl/ok.tpl',
+          notOk: 'tpl/notOk.tpl',
+          soundOk: 'sounds/ok.mp3',
+          soundNotOk: 'sounds/notOk.mp3'
+        };
+      };*/
+
+      /*$scope.validateToPuppa = function() {
         var el = angular.element($scope.submit);
 
         $s = el.scope();
@@ -27,8 +62,8 @@
 
           $compile(el)($s);
         });
-      }
-    });
+      }*/
+  });
 
   document.addEventListener('click', function() {
     var alert = document.getElementById('alert');
